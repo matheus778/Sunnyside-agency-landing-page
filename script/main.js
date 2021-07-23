2d72c80bd36131db83487539cb95e476ab6f4b07
matheus778 <matheuzv777@gmail.com>
2021-07-23 04:11:34 -0300

    adicionado efeitos de scroll

--- script/main.js
+++ script/main.js
@@ -4,3 +4,28 @@ const menuContainer = document.querySelector('.menu')
 menu.addEventListener('click', () => {
   menuContainer.classList.toggle('show')
 })
+
+// scrollReveal
+const scrollReveal = ScrollReveal({
+  reset: true,
+  distance: '70px',
+  duration: 1000,
+  origin: 'bottom'
+})
+
+scrollReveal.reveal(
+  `.menu-container,
+.header-text,
+.egg-image,
+.text1,
+.taca-image,
+.depoimento-container,
+.leite-image,
+.laranja-image-mosaico,
+.sorvete-image,
+.acucar-img,
+#mosaico,
+.attribution`,
+
+  { interval: 100 }
+)
